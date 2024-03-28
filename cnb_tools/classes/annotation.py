ea0d4e5480647778506a180ba3addafdec7d0857
verena <9377970+vpchung@users.noreply.github.com>
2024-03-27 17:03:20 -0700

    add logic for adding/updating annotations

--- cnb_tools/classes/annotation.py
+++ cnb_tools/classes/annotation.py
@@ -1,18 +1,19 @@
 """Class representing annotations of a challenge submission."""
 
 import sys
+import json
 
 from synapseclient.core.exceptions import SynapseHTTPError
+from synapseclient.core.retry import with_retry
 
 from cnb_tools.classes.base import SynapseBase
 
 
 class Annotation(SynapseBase):
     def __init__(self, sub_id):
-        super().__init__()
-        self.sub_id = sub_id
+        super().__init__(sub_id)
         try:
-            self.annotations = self.syn.getSubmissionStatus(sub_id)
+            self.curr_annotations = self.syn.getSubmissionStatus(sub_id)
         except SynapseHTTPError as err:
             print(
                 f"⛔ {err.response.json().get('reason')}. "
@@ -20,13 +21,34 @@ class Annotation(SynapseBase):
             )
             sys.exit(1)
 
-    def update(self):
-        pass
+    def _annotate(self, new_annots, verbose) -> None:
+        self.curr_annotations.submissionAnnotations.update(new_annots)
+        curr_annotations = self.syn.store(self.curr_annotations)
+        print(f"Submission ID {self.uid} annotations updated.")
 
-    def reset(self):
-        pass
+        if verbose:
+            print(f"Annotations:")
+            print(json.dumps(curr_annotations.submissionAnnotations, indent=2))
 
-    def update_status(self, new_status):
-        self.annotations.status = new_status
-        self.syn.store(self.annotations)
-        print(f"Updated submission ID {self.sub_id} to status: {new_status}")
+    def update(self, anots_file, verbose) -> None:
+        with open(anots_file, encoding="utf-8") as f:
+            new_annots = json.load(f)
+
+        # Filter annotations with null and empty-list values
+        new_annots = {
+            key: value
+            for key, value in new_annots.items()
+            if value not in [None, []]
+        }
+        new_annots = with_retry(
+            lambda: self._annotate(new_annots, verbose),
+            wait=3,
+            retries=10,
+            retry_status_codes=[412, 429, 500, 502, 503, 504],
+            verbose=True,
+        )
+
+    def update_status(self, new_status) -> None:
+        self.curr_annotations.status = new_status
+        self.syn.store(self.curr_annotations)
+        print(f"Updated saubmission ID {self.uid} to status: {new_status}")
